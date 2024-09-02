f65e2c4372ff11a24a7e117619293b48127e34b1
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-09-02 15:53:47 +0900

    set up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
 
 function App() {
-  return <div></div>;
+  const isLoggedIn = false;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route path="/example">
+          <h1>Ex</h1>
+          {!isLoggedIn ? "Plz, login" : null}
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
