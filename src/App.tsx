2ca1ddb30c4c84efe1962b6094956192c5816baf
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-09-03 17:39:59 +0900

    not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
   const isLoggedIn = false;
@@ -13,6 +19,9 @@ function App() {
         <Route path="/example">
           <h1>Ex</h1>
           {!isLoggedIn ? "Plz, login" : null}
+        </Route>
+        <Route>
+          <NotFound />
         </Route>
       </Switch>
     </Router>
