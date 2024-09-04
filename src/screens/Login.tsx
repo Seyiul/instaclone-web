748bafb276c4cdd11ebc345024fb54d2a22a7c38
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-09-04 14:17:53 +0900

    styled component setup:

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,13 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { isLoggedInVar } from "../apollo";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+interface ITitleProps {
+  potato: boolean;
+}
+
 type LoginProps = {};
 
+const Title = styled.h1<ITitleProps>`
+  color: ${(props) => (props.potato ? "tomato" : "palevioletred")};
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+`;
+
+const Container = styled.div`
+  background-color: bisque;
+`;
+
+const Button = styled.button`
+  color: violet;
+`;
+
 const Login: React.FC<LoginProps> = ({}) => {
+  const [potato, setPotato] = useState<boolean>(false);
+  const togglePotato = () => setPotato((current: boolean) => !current);
+
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={() => isLoggedInVar(true)}>Log in now!</button>
-    </div>
+    <Container>
+      <Title potato={potato}>Login</Title>
+      <Button onClick={togglePotato}>Toggle!</Button>
+      <Button onClick={() => isLoggedInVar(true)}>Log in now!</Button>
+    </Container>
   );
 };
 
