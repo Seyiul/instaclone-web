336874edf4f353c6b855897321b1f96408f97c5f
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-09-04 14:37:05 +0900

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,39 @@ import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
 import { useReactiveVar } from "@apollo/client";
-import { isLoggedInVar } from "./apollo";
+import { darkModeVar, isLoggedInVar } from "./apollo";
+import { ThemeProvider } from "styled-components";
+
+const lightTheme = {
+  fontColor: "#2c2c2c",
+  bgColor: "lightgray",
+};
+
+const darkTheme = {
+  fontColor: "lightgray",
+  bgColor: "#2c2c2c",
+};
 
 const App: React.FC = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          {isLoggedIn ? <Home /> : <Login />}
-        </Route>
-        <Route path="/example">
-          <h1>Example Page</h1>
-          {!isLoggedIn && <p>Please, log in</p>}
-        </Route>
-        <Route>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route path="/example">
+            <h1>Example Page</h1>
+            {!isLoggedIn && <p>Please, log in</p>}
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
 
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { isLoggedInVar } from "../apollo";
+import { darkModeVar, isLoggedInVar } from "../apollo";
 import styled from "styled-components";
-import PropTypes from "prop-types";
 
 interface ITitleProps {
   potato: boolean;
@@ -10,13 +9,13 @@ interface ITitleProps {
 type LoginProps = {};
 
 const Title = styled.h1<ITitleProps>`
-  color: ${(props) => (props.potato ? "tomato" : "palevioletred")};
+  color: ${(props) => props.theme.fontColor};
   font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
     Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
 `;
 
 const Container = styled.div`
-  background-color: bisque;
+  background-color: ${(props) => props.theme.bgColor};
 `;
 
 const Button = styled.button`
@@ -30,7 +29,8 @@ const Login: React.FC<LoginProps> = ({}) => {
   return (
     <Container>
       <Title potato={potato}>Login</Title>
-      <Button onClick={togglePotato}>Toggle!</Button>
+      <Button onClick={() => darkModeVar(true)}>To Dark</Button>
+      <Button onClick={() => darkModeVar(false)}>To Light</Button>
       <Button onClick={() => isLoggedInVar(true)}>Log in now!</Button>
     </Container>
   );
