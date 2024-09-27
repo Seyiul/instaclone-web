9ee9668742c0863b8a4ec35aa748bb9840d2ad7a
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-09-27 17:58:48 +0900

    parsing hashtags

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,3 +1,4 @@
+import sanitizeHtml from "sanitize-html";
 import styled from "styled-components";
 import { FatText } from "../shared";
 
@@ -7,6 +8,15 @@ const SComment = styled.div`
 
 const CommentCaption = styled.span`
   margin-left: 10px;
+
+  mark {
+    background-color: inherit;
+    color: ${(props) => props.theme.accent};
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
 `;
 interface CommentProps {
   author: string;
@@ -14,10 +24,22 @@ interface CommentProps {
 }
 
 function Comment({ author, payload }: CommentProps) {
+  const cleanPayload = sanitizeHtml(
+    payload.replace(/#[\d|A-Z|a-z|ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+/g, "<mark>$&</mark>"),
+
+    {
+      allowedTags: ["mark"],
+    }
+  );
+
   return (
     <SComment>
       <FatText>{author}</FatText>
-      <CommentCaption>{payload}</CommentCaption>
+      <CommentCaption
+        dangerouslySetInnerHTML={{
+          __html: cleanPayload,
+        }}
+      ></CommentCaption>
     </SComment>
   );
 }
