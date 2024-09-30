76ffb279e25af03530ae43ec89b5835a27373e4b
Lee Seul Yi <63714074+Seyiul@users.noreply.github.com>
2024-09-30 18:00:42 +0900

    profile screen test

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,6 +1,21 @@
 import { gql, useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
 import { PHOTO_FRAGMENT } from "../fragments";
+import Avatar from "../components/Avatar";
+import styled from "styled-components";
+const InfoContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const ProfileInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Username = styled.div``;
+const FollowInfo = styled.div``;
+const Bio = styled.div``;
 
 interface ParamTypes {
   username: string;
@@ -35,7 +50,21 @@ function Profile() {
   });
 
   console.log(data);
-  return <>{username}</>;
+  return (
+    <>
+      <InfoContainer>
+        <Avatar url={data.seeProfile.avatar} />
+        <ProfileInfo>
+          <Username>{data.seeProfile.username}</Username>
+          <FollowInfo>
+            <span>{data.seeProfile.totalFollowers}followers</span>
+            <span>{data.seeProfile.totalFollowing}following</span>
+          </FollowInfo>
+          <Bio>{data.seeProfile.bio}</Bio>
+        </ProfileInfo>
+      </InfoContainer>
+    </>
+  );
 }
 
 export default Profile;
